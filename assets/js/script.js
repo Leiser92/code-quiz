16555c063b38aec56c73eb3a5a6fee0f49b620d0
Leiser <mleiser92@gmail.com>
2021-12-02 17:50:53 -0700

    added quiz questions and laid out functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,73 @@
 //global variables
 
-
+//questions
+let questions = [
+   
+    {
+        question: "Inside which HTML element do we put the Javascript?",
+        answer: "<script>",
+        options: [
+            "<javascript>",
+            "<script>",
+           "<scripting>" ,
+           "<js>"
+        ]
+    },
+    {
+        question: "Which built-in method returns the calling string value converted to upper case?",
+        answer: "toUpperCase()",
+        options: [
+           "toUpperCase()" ,
+            "toUpper()",
+            "changeCase(case)",
+            "None of the above.",
+        ]
+    },
+    {
+        question: "Which of the following type of variable is visible only within a function where it is defined?",
+        answer: "local variable",
+        options: [
+            "global variable",
+           "local variable" ,
+           "Both of the above" ,
+           " None of the above"
+        ]
+    },
+    {
+        question: "Using a period between the object name and property is called what?",
+        answer: "Dot Notation",
+        options: [
+         "Dot Notation",
+         "Dot Conjunction",
+         "Dot Addition",
+         "Dot Function"
+         
+        ]
+    },
+];
 
 
 //timer
+function setTimer() {
 
+}
 
 
 //remove start button
+function removeStart(){
 
+}
 
 
 //start quiz
+function startQuiz () {
 
+}
 
-
-//questions
 
 
 
 //end quiz
+function endQuiz() {
+
+}
